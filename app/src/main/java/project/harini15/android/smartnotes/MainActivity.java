78947f7d9afebe594432e0233eff4e9e5074dea9
DEV <dev16011996@gmail.com>
2016-10-08 22:46:22 +0530

    Initial commit

---
+++ app/src/main/java/project/harini15/android/smartnotes/MainActivity.java
@@ -0,0 +1,116 @@
+package project.harini15.android.smartnotes;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListNotes;
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListNotes = (ListView) findViewById(R.id.main_listview);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_create: //run NoteActivity in new note mode
+                startActivity(new Intent(this, NoteActivity.class));
+                break;
+
+            case R.id.action_settings:
+                //TODO show settings activity
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //load saved notes into the listview
+        //first, reset the listview
+        mListNotes.setAdapter(null);
+        ArrayList<Note> notes = Utilities.getAllSavedNotes(getApplicationContext());
+
+        //sort notes from new to old
+        Collections.sort(notes, new Comparator<Note>() {
+            @Override
+            public int compare(Note lhs, Note rhs) {
+                if(lhs.getDateTime() > rhs.getDateTime()) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+        if(notes != null && notes.size() > 0) { //check if we have any notes!
+            final NoteAdapter na = new NoteAdapter(this, R.layout.view_note_item, notes);
+            mListNotes.setAdapter(na);
+
+            //set click listener for items in the list, by clicking each item the note should be loaded into NoteActivity
+            mListNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    //run the NoteActivity in view/edit mode
+                    String fileName = ((Note) mListNotes.getItemAtPosition(position)).getDateTime()
+                            + Utilities.FILE_EXTENSION;
+                    Intent viewNoteIntent = new Intent(getApplicationContext(), NoteActivity.class);
+                    viewNoteIntent.putExtra(Utilities.EXTRAS_NOTE_FILENAME, fileName);
+                    startActivity(viewNoteIntent);
+                }
+            });
+        } else { //remind user that we have no notes!
+            Toast.makeText(getApplicationContext(), "You have NO SAVED NOTES!\nCreate some NEW NOTES :)"
+                    , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to EXIT", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+
+            }
+        }, 2000);
+    }
+
+}
+
